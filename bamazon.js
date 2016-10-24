5053fca7d45e36b19b4ed66f93b45dcb3355b894
Joshua Hudson <joshudson93@gmail.com>
2016-10-24 19:01:39 -0400

    First prompt works. Working on the second

---
+++ bamazon.js
@@ -0,0 +1,90 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", //Your username
+    password: "root", //Your password
+    database: "bamazon"
+})
+
+connection.connect(function(err){
+	if (err) throw err;
+	// console.log("connected as id" + connection.threadId);
+})
+
+// inquirer.prompt({
+//         name: "number",
+//         message:"What is the ID number of the product that you'd like to buy?"
+        
+//     }).then(function(answer) {
+       
+//        console.log(answer);
+//        })
+
+
+connection.query('SELECT * FROM Products', function(err, res) {
+	for(var i = 0; i < res.length; i++){
+     console.log(res[i].id + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity)
+	}
+          // var match = res[i].id;
+          // console.log(match);
+   // console.log(match)
+
+// }
+
+// 	console.log("================================================");
+// })
+
+})
+
+
+
+   inquirer.prompt({
+        name: "number",
+        message:"What is the ID number of the product that you'd like to buy?"
+        
+       }).then(function(answer) {
+
+   
+connection.query('SELECT * FROM Products WHERE id=?', [answer.number], function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity);
+        
+        inquirer.prompt({
+        name: "quantity",
+        message:"How many units of the product would you like to purchase?"
+        
+       }).then(function(answer) {
+
+          if(answer < res[i].StockQuantity){
+                  console.log("Leave some product for everyone else geez!")
+          }
+          else{
+          	console.log(" Boom! " + answer.quantity + " of " + res[i].ProductName + " coming your way." )
+          }
+
+           })
+    }
+
+    // console.log(answer);
+
+})
+
+           })
+
+
+    // inquirer.prompt({
+    //     name: "quantity",
+    //     message:"How many units of the product would you like to purchase?"
+        
+    //    }).then(function(answer) {
+
+          
+
+    //        })
+
+
+ 
+
